dd12afdd6639a3348e6d7fa20221347d5721d10d
SirPotatoIV <otoolej33@gmail.com>
2019-12-08 12:56:10 -0600

    Created new function and used deconstruction to make all the values from the github api response I care about into constants.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,8 @@
 function generatePDFofGitHubProfile(){
     // Have the application be invoked with sh node index.js ?????
     // Bring in npms (axios, inquirer, and probably one for a pdf)
-    // function createModuleVariables() {
-        const axios = require('axios');
-        const inquirer = require('inquirer');
-    // }
-    // createModuleVariables();
+    const axios = require('axios');
+    const inquirer = require('inquirer');
     // Get a github username and color from prompts
     function promptUser(){
         // https://github.com/SBoudrias/Inquirer.js/blob/master/packages/inquirer/examples/input.js
@@ -46,17 +43,38 @@ function generatePDFofGitHubProfile(){
         const githubUrlRequest = `https://api.github.com/users/${githubUsername}`; 
         
         // Taken from axios module README
+        // https://developer.github.com/v3/users/
         axios.get(githubUrlRequest)
-            .then(function (response) {
+            .then(function ({data}) {
                 // handle success
-                console.log(response);
+                // console.log(data);
+                const githubProfileData = data;
+                generateResumeHtml(githubProfileData)
             })
             .catch(function (error) {
                 // handle error
                 console.log(error);
             });
     }
-    // getUserProfile();
+
+    function generateResumeHtml(githubProfileData){
+        // * Profile image - avatar_url
+        // * User name - login
+        // * Links to the following:
+        //   * User location via Google Maps - location
+        //   * User GitHub profile - html_url
+        //   * User blog - blog
+        // * User bio - bio
+        // * Number of public repositories - public_repos
+        // * Number of followers - followers
+        // * Number of GitHub stars - starred_url 
+        // * -- Need to create function to count the stars.
+        // * Number of users following - following
+        const {avatar_url, login, location, html_url, blog, bio, public_repos, followers, following} = githubProfileData;
+        console.log(avatar_url, login, location, html_url, blog, bio, public_repos, followers, following);
+
+    }
+    
 }
 generatePDFofGitHubProfile();
     // Organize the github profile information for use in generating the pdf
