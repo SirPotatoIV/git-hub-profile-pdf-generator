f7dae17282489c8d89de5f747a0e5c74cad60661
SirPotatoIV <otoolej33@gmail.com>
2019-12-08 11:45:37 -0600

    Added code to require modules axios and inquirer. Created function to get a users repos based on the in-class activities.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,23 @@
-console.log('hello!')
+// console.log('hello!')
+const axios = require('axios');
+const inquirer = require('inquirer');
+
+// Taken from axios module README
+// Make a request for a GitHub users profile with a given ID
+// https://developer.github.com/v3/repos/contents/
+
+function getUserProfile() {
+    const githubUsername = "SirPotatoIV";
+    const githubUrlRequest = `https://api.github.com/users/${githubUsername}/repos?per_page=100`; 
+
+    axios.get(githubUrlRequest)
+        .then(function (response) {
+            // handle success
+            console.log(response);
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        });
+}
+getUserProfile();
