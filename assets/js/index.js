a6121f9c195f665145d654a244357dcb3e87c8d2
SirPotatoIV <otoolej33@gmail.com>
2019-12-08 11:53:31 -0600

    Added psuedo code. Should have done this earlier, but better late than never.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,21 @@
-// console.log('hello!')
+// Have the application be invoked with sh node index.js ?????
+// Bring in npms (axios, inquirer, and probably one for a pdf)
+// Get a github username and color from prompts
+// Get the provided username github profile information
+// Organize the github profile information for use in generating the pdf
+// Generate HTML to create the content for the pdf
+// Generate the PDF
+
+
 const axios = require('axios');
 const inquirer = require('inquirer');
 
 // Taken from axios module README
 // Make a request for a GitHub users profile with a given ID
 // https://developer.github.com/v3/repos/contents/
-
 function getUserProfile() {
     const githubUsername = "SirPotatoIV";
-    const githubUrlRequest = `https://api.github.com/users/${githubUsername}/repos?per_page=100`; 
+    const githubUrlRequest = `https://api.github.com/users/${githubUsername}`; 
 
     axios.get(githubUrlRequest)
         .then(function (response) {
