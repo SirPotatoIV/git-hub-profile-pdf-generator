6705bcb7557bfa8b1864a1aba958adf94e1499d4
SirPotatoIV <otoolej33@gmail.com>
2019-12-08 12:14:00 -0600

    Created new function to prompt the user for input that will be used to generate the resume. Succesfully prompted, saved answers, and console.log'd answers.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,30 +1,66 @@
-// Have the application be invoked with sh node index.js ?????
-// Bring in npms (axios, inquirer, and probably one for a pdf)
-// Get a github username and color from prompts
-// Get the provided username github profile information
-// Organize the github profile information for use in generating the pdf
-// Generate HTML to create the content for the pdf
-// Generate the PDF
+function generatePDFofGitHubProfile(){
+    // Have the application be invoked with sh node index.js ?????
+    // Bring in npms (axios, inquirer, and probably one for a pdf)
+    // function createModuleVariables() {
+        const axios = require('axios');
+        const inquirer = require('inquirer');
+    // }
+    // createModuleVariables();
+    // Get a github username and color from prompts
+    function promptUser(){
+        // https://github.com/SBoudrias/Inquirer.js/blob/master/packages/inquirer/examples/input.js
+        questions = [
+            {
+              type: 'input',
+              name: 'username',
+              message: "Enter GitHub username of user you would like to generate a Resume from."
+            },
+            {
+              type: 'input',
+              name: 'color',
+              message: "Choose a background color for the generated PDF",
+              default: function() {
+                return 'blue';
+              }
+            }
+        ];
+        
+        // Help from inquirer README
+        inquirer
+            .prompt(
+                /* Pass your questions in here */
+                questions
+            )
+            .then(answers => {
+                // Use user feedback for... whatever!!
+                console.log(JSON.stringify(answers, null, ' '));
+            });
+    }
+    promptUser();
+    // Get the provided username github profile information
+    // Organize the github profile information for use in generating the pdf
+    // Generate HTML to create the content for the pdf
+    // Generate the PDF
 
 
-const axios = require('axios');
-const inquirer = require('inquirer');
 
-// Taken from axios module README
-// Make a request for a GitHub users profile with a given ID
-// https://developer.github.com/v3/repos/contents/
-function getUserProfile() {
-    const githubUsername = "SirPotatoIV";
-    const githubUrlRequest = `https://api.github.com/users/${githubUsername}`; 
-
-    axios.get(githubUrlRequest)
-        .then(function (response) {
-            // handle success
-            console.log(response);
-        })
-        .catch(function (error) {
-            // handle error
-            console.log(error);
-        });
+    // Make a request for a GitHub users profile with a given ID
+    // Followed class activity for some help
+    function getUserProfile() {
+        const githubUsername = "SirPotatoIV";
+        const githubUrlRequest = `https://api.github.com/users/${githubUsername}`; 
+        
+        // Taken from axios module README
+        axios.get(githubUrlRequest)
+            .then(function (response) {
+                // handle success
+                console.log(response);
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            });
+    }
+    // getUserProfile();
 }
-getUserProfile();
+generatePDFofGitHubProfile();
